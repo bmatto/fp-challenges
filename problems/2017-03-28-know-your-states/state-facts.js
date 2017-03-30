d49e4c2c5b666a51cb1784d0b0a58e5bc713c20c
Byron Matto <byronmatto@libp45p-122561r.lm.lmig.com>
2017-03-30 15:14:37 -0400

    Initial solution for second problem

--- problems/2017-03-28-know-your-states/state-facts.js
+++ problems/2017-03-28-know-your-states/state-facts.js
@@ -1,10 +1,42 @@
+const ratio = (a, b) => a / b
+const stateDensity = state => ratio(state.population, state.totalArea)
+const wetness = state => ratio(state.landArea, state.waterArea)
+const difference = (a, b) => a - b
+const prop = (prop, object) => object[prop]
+const head = list => list[0]
+const stateName = state => prop('state', state)
+const reverse = list => [...list].reverse()
+const getCapitalCity = state => prop('capitalCity', state)
+const getLargestCity = state => prop('largestCity', state)
+const isCapitalLargest = state => getCapitalCity(state) === getLargestCity(state)
+const mapDifference = (a, b, func) => difference(func(a), func(b))
+
+const sortByDensity = dataSet => {
+  return dataSet.sort(
+    (a, b) => mapDifference(a, b, stateDensity)
+  )
+}
+
+const sortByWetness = dataSet => {
+  return dataSet.sort(
+    (a, b) => mapDifference(a, b, wetness)
+  )
+}
+
 const stateFacts = function(dataSet) {
-  console.info('Capitals of each US state = ');
-  console.info('Most densily-populated state = ');
-  console.info('Least densily-populated state = ');
-  console.info('State(s) with one city as capital and largest population = ');
-  console.info('States by their land-area / water-area ratio = ');
-  console.info('[Insert your own question here] = [insert your own answer here]');
+  const statesSortedByDensity = sortByDensity(dataSet)
+  const mostDenseState = stateName(head(reverse(statesSortedByDensity)))
+  const leastDenseState = stateName(head(statesSortedByDensity))
+
+  const statesByWetness = sortByWetness(dataSet)
+
+
+  console.info(`Capitals of each US state = ${dataSet.map(getCapitalCity)}`);
+  console.info(`Most densily-populated state = ${mostDenseState}`);
+  console.info(`Least densily-populated state = ${leastDenseState}`);
+  console.info(`State(s) with one city as capital and largest population = ${dataSet.filter(isCapitalLargest).map(stateName)}`);
+  console.info(`States by their land-area / water-area ratio = ${statesByWetness.map(stateName)}`);
+  console.info(`[Insert your own question here] = [insert your own answer here]`);
 };
 
 module.exports = stateFacts;
